458ad4b58b72166e4ece22100091cceecf768660
shukrullojonappdev <shukrullojon.dev@gmail.com>
2023-03-01 01:22:56 +0500

    edited

--- backend/api/api/settings.py
+++ backend/api/api/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
     'accounts.apps.AccountsConfig',
     'books.apps.BooksConfig',
     'categories.apps.CategoriesConfig',
--- backend/api/api/urls.py
+++ backend/api/api/urls.py
@@ -3,6 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('rest_framework.urls')),
     path('api/', include('accounts.urls'))
 ]
