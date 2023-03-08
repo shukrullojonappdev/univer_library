edbe61667f0d170daf3da2ffd13475d7ee17cca5
shukrullojonappdev <shukrullojon.dev@gmail.com>
2023-03-08 19:24:38 +0500

    изменено структура html файлов

--- app/api/api/views.py
+++ app/api/api/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+from books.models import Book
+
 @login_required(login_url='/accounts/login/')
 def home(request):
-    return render(request, 'home/home.html',)
+    books = Book.objects.all()
+    context = {
+        'books': books
+    }
+    print(context)
+    return render(request, 'pages/home.html', context)
